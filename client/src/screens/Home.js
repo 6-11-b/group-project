e5d6f0bea280dd30bb9b6a5b6e11f37824b6d540
Jessica Brown <gbandjh32@yahoo.com>
2018-07-01 23:27:22 -0700

    added screens and user signup and login forms

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
-const Home = () =>
+const Home = () => <Navbar title="Home" />;
     <div>
         <h1>Home</h1>
         <p>The html goes here</p>
