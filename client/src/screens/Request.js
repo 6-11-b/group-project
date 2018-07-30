d52548d50499305e89c3046dca4894e92027576a
tengu707 <33167542+tengu707@users.noreply.github.com>
2018-07-30 14:15:28 -0700

    made maintenance requests updatable and deletable

--- client/src/screens/Request.js
+++ client/src/screens/Request.js
@@ -4,30 +4,66 @@ import Navbar from '../components/Navbar';
 
 
 class Request extends React.Component {
-    state = {
-        response: ''
-      };
+    constructor() {
+        super();
+        this.state = {
+            property: [],
+            string: []
+        };
+        this.mapArray = this.mapArray.bind(this);
+        this.callApi = this.callApi.bind(this);
+    }
     
-      componentDidMount() {
+    componentDidMount() {
         this.callApi()
-          .then(res => this.setState({ response: res }))
-          .catch(err => console.log(err));
-      }
+            .then( res => this.mapArray(res.maintenance)
+                .then( console.log(res) ) 
+            )
+            .then( console.log("Loaded") )
+            .catch( err => console.log(err) )
+    }
     
-      callApi = async () => {
-        const response = await fetch('/api/hello');
+    callApi = async () => {
+        const response = await fetch('/api/request');
         const body = await response.json();
     
-        if (response.status !== 200) throw Error(body.message);
+        if (response.status !== 200) throw Error("error");
     
         return body;
-      };
+    };
+
+    mapArray(array) {
+        let localString =
+            <form id="request" action="/api/request/edit" method="POST">
+                <label for='firstlastname'>Enter your First and Last name: </label>
+                <input type="text" name="firstlastname" id="firstlastname" value={array[0].name}/>
+                <br />
+                <label for='email'>Enter your email address: </label>
+                <input type="email" name="email" id="email" value={array[0].email}/>
+                <br />
+                <label for='address'>Enter the address for your rental property: </label>
+                <input type="text" name="address" id="address" value={array[0].address}/>
+                <br />
+                <label for='date'>Enter the date of request: </label>
+                <input type="date" name="date" id="date" value={array[0].date}/>
+                <br />
+                <p style={{fontSize: 20, fontWeight: "bold"}}>Please list the problem or repair needed for your rental property</p>
+                <label for='maintenancerequest'>Maintenance Request </label>
+                <input type="text" name="maintenancerequest" id="maintenance" defaultValue={array[0].maintenancerequest}/>
+                
+                <button type="submit">Edit Request</button>
+                <button type="submit" formaction="/api/request/delete">Delete Request</button>
+            </form>
+        ;
+        this.setState({ string: localString });
+        console.log(localString);
+    };
 
     render() {
     return (
         <div>
             <Navbar title="Maintenance Request" />
-            <h1>{this.state.response.message}</h1>
+            {this.state.string}
             <h1>Please fill out the following form to submit a request for maintenance for your rental property</h1>
             <form id="request" action="/api/request" method="POST">
                 <input type="radio" name="user" value="tenant" />Tenant
